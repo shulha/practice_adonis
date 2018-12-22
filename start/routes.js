f5bfaead06cfdba2dafe743e4da735e63d51bc6d
shulha <shulhad@gmail.com>
2018-12-22 14:03:17 +0200

    feat(core): add routes and AuthController

--- start/routes.js
+++ start/routes.js
@@ -15,3 +15,6 @@
 const Route = use('Route');
 
 Route.get('/', () => ({ status: 'Ok', version: '1.0.0' }));
+
+Route.post('/login', 'AuthController.login');
+Route.post('/logout', 'AuthController.logout');
